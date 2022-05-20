bfd6abe07d8340ca99e6e038247e7b6a2acf3560
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-05-20 19:49:36 +0530

    add bloom quality manipulation

--- MBExtender/plugins/GraphicsExtension/BloomPass.cpp
+++ MBExtender/plugins/GraphicsExtension/BloomPass.cpp
@@ -23,17 +23,30 @@ void BloomPass::processPass(Point2I extent) {
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+
 	// bind shader and texture uniform
 	if (!glIsEnabled(GL_TEXTURE_2D))
 		glEnable(GL_TEXTURE_2D);
 
 	// Pass 1
 
+	int bloomQualityLevel = TGE::Con::getIntVariable("$pref::Video::ShapeBloomQuality");
+
+	if (bloomQualityLevel == 0) {
+		if (glIsEnabled(GL_TEXTURE_2D))
+			glDisable(GL_TEXTURE_2D);
+
+		glPopMatrix();
+		return;
+	}
+
 	this->shader->activate();
 	this->shader->setUniformLocation("textureSampler", 0);
 	this->shader->setUniformLocation("depthSampler", 1);
 	this->shader->setUniformLocation("bloomDepthSampler", 2);
 	glUniform1ui(this->shader->getUniformLocation("compareDepth"), 1);
+	glUniform1i(this->shader->getUniformLocation("bloomQuality"), bloomQualityLevel);
+
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, this->colorBuffer);
 	glActiveTexture(GL_TEXTURE1);
@@ -53,67 +66,72 @@ void BloomPass::processPass(Point2I extent) {
 	GL_CheckErrors("draw quad");
 	this->shader->deactivate();
 
-	// Pass 2-5
+	if (bloomQualityLevel > 1) {
 
-	for (int i = 0; i < 2; i++) {
-		glBindFramebuffer(GL_FRAMEBUFFER, this->frameBuffer);
-		GL_CheckErrors("render bloom: 2");
-		glClearColor(0, 0, 0, 0);
-		glClear(GL_COLOR_BUFFER_BIT);
+		// Pass 2-5
 
-		this->shader->activate();
-		this->shader->setUniformLocation("textureSampler", 0);
-		this->shader->setUniformLocation("depthSampler", 1);
-		this->shader->setUniformLocation("bloomDepthSampler", 2);
-		glUniform1ui(this->shader->getUniformLocation("compareDepth"), 0);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, this->finalColorBuffer);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, gFBO->depthTextureHandle);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, this->finalDepthBuffer);
-		glUniform2f(this->shader->getUniformLocation("screenSize"), static_cast<F32>(extent.x), static_cast<F32>(extent.y));
-		GL_CheckErrors("activate bloom shader");
+		for (int i = 0; i < 2; i++) {
+			glBindFramebuffer(GL_FRAMEBUFFER, this->frameBuffer);
+			GL_CheckErrors("render bloom: 2");
+			glClearColor(0, 0, 0, 0);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-		// send verts
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, gFBO->quadVBO);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-		GL_CheckErrors("send verts");
-		// Draw the quad! Remember, it's 2 triangles!
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		GL_CheckErrors("draw quad");
-		this->shader->deactivate();
+			this->shader->activate();
+			this->shader->setUniformLocation("textureSampler", 0);
+			this->shader->setUniformLocation("depthSampler", 1);
+			this->shader->setUniformLocation("bloomDepthSampler", 2);
+			glUniform1ui(this->shader->getUniformLocation("compareDepth"), 0);
+			glUniform1i(this->shader->getUniformLocation("bloomQuality"), bloomQualityLevel);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, this->finalColorBuffer);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, gFBO->depthTextureHandle);
+			glActiveTexture(GL_TEXTURE2);
+			glBindTexture(GL_TEXTURE_2D, this->finalDepthBuffer);
+			glUniform2f(this->shader->getUniformLocation("screenSize"), static_cast<F32>(extent.x), static_cast<F32>(extent.y));
+			GL_CheckErrors("activate bloom shader");
 
-		// Pass 3
-		glBindFramebuffer(GL_FRAMEBUFFER, this->finalFrameBuffer);
-		GL_CheckErrors("render bloom: 2");
-		glClearColor(0, 0, 0, 0);
-		glClear(GL_COLOR_BUFFER_BIT);
+			// send verts
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, gFBO->quadVBO);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			GL_CheckErrors("send verts");
+			// Draw the quad! Remember, it's 2 triangles!
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			GL_CheckErrors("draw quad");
+			this->shader->deactivate();
 
-		this->shader->activate();
-		this->shader->setUniformLocation("textureSampler", 0);
-		this->shader->setUniformLocation("depthSampler", 1);
-		this->shader->setUniformLocation("bloomDepthSampler", 2);
-		glUniform1ui(this->shader->getUniformLocation("compareDepth"), 0);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, this->colorBuffer);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, gFBO->depthTextureHandle);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, this->depthBuffer);
-		glUniform2f(this->shader->getUniformLocation("screenSize"), static_cast<F32>(extent.x), static_cast<F32>(extent.y));
-		GL_CheckErrors("activate bloom shader");
+			// Pass 3
+			glBindFramebuffer(GL_FRAMEBUFFER, this->finalFrameBuffer);
+			GL_CheckErrors("render bloom: 2");
+			glClearColor(0, 0, 0, 0);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-		// send verts
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, gFBO->quadVBO);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-		GL_CheckErrors("send verts");
-		// Draw the quad! Remember, it's 2 triangles!
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		GL_CheckErrors("draw quad");
-		this->shader->deactivate();
+			this->shader->activate();
+			this->shader->setUniformLocation("textureSampler", 0);
+			this->shader->setUniformLocation("depthSampler", 1);
+			this->shader->setUniformLocation("bloomDepthSampler", 2);
+			glUniform1ui(this->shader->getUniformLocation("compareDepth"), 0);
+			glUniform1i(this->shader->getUniformLocation("bloomQuality"), bloomQualityLevel);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, this->colorBuffer);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, gFBO->depthTextureHandle);
+			glActiveTexture(GL_TEXTURE2);
+			glBindTexture(GL_TEXTURE_2D, this->depthBuffer);
+			glUniform2f(this->shader->getUniformLocation("screenSize"), static_cast<F32>(extent.x), static_cast<F32>(extent.y));
+			GL_CheckErrors("activate bloom shader");
+
+			// send verts
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, gFBO->quadVBO);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			GL_CheckErrors("send verts");
+			// Draw the quad! Remember, it's 2 triangles!
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			GL_CheckErrors("draw quad");
+			this->shader->deactivate();
+		}
 	}
 
 
