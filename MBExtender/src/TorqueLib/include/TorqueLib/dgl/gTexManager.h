562fd3cab235d2b0c9ecdc06683e5dcf3b1e5491
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-05-28 14:14:57 +0530

    fucc

--- MBExtender/src/TorqueLib/include/TorqueLib/dgl/gTexManager.h
+++ MBExtender/src/TorqueLib/include/TorqueLib/dgl/gTexManager.h
@@ -84,8 +84,8 @@ namespace TGE
 		FN(TGE::TextureObject *, registerTexture, (const char *textureName, const GBitmap *data, bool clampToEdge), 0x45A8D0_win, 0x67BD0_mac);
 		FN(bool, createGLName, (TGE::GBitmap *pBitmap, bool clampToEdge, U32 firstMip, U32 type, TGE::TextureObject *to), 0x40265D_win, 0x66570_mac);
 		FN(TGE::GBitmap *, createPaddedBitmap, (TGE::GBitmap *pBitmap), 0x405641_win, 0x66270_mac);
-		FN(void, preDestroy, (), 0x406C44_win);
-		FN(void, destroy, (), 0x404651_win);
+		FN(void, preDestroy, (), 0x406C44_win, 0x67AD0_mac);
+		FN(void, destroy, (), 0x404651_win, 0x67A70_mac);
 	};
 
 	GLOBALVAR(bool, sgTextureTrilinear, 0x6A7621_win, 0x2DA890_mac);
